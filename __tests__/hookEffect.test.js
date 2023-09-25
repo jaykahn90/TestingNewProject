0232b2077bac7f24dc3895d8c65d5ebfa9c0d72d
jalal khan <j.k90@icloud.com>
2023-09-25 11:23:21 +1000

    Included Original API-Fetch & commented out.

--- __tests__/hookEffect.test.js
+++ __tests__/hookEffect.test.js
@@ -35,3 +35,43 @@ describe('<HookEffect />', () => {
     );
   });
 });
+
+/////////////////////Testing Original API-Fetch /////////////////////////
+
+// import React from 'react';
+// import {render} from '@testing-library/react-native';
+// import HookEffect from '../src/screens/HookEffect';
+
+// global.fetch = jest.fn();
+
+// describe('<HookEffect />', () => {
+//   beforeEach(() => {
+//     fetch.mockClear();
+//   });
+
+//   it('fetches and displays user data correctly', async () => {
+//     fetch.mockResolvedValueOnce({
+//       json: async () => [
+//         {
+//           id: '1',
+//           image:
+//             'https://images.pexels.com/photos/851238/pexels-photo-851238.jpeg?auto=compress&cs=tinysrgb&w=1600',
+//           name: 'iSeekBlinds',
+//           email: 'iSeekBlinds@abc.com',
+//           mobile: '1234567899',
+//         },
+//       ],
+//     });
+
+//     const {findByText} = render(<HookEffect />);
+
+//     const nameElement = await findByText('Name: iSeekBlinds');
+//     expect(nameElement).toBeTruthy();
+
+//     const emailElement = await findByText('Email: iSeekBlinds@abc.com');
+//     expect(emailElement).toBeTruthy();
+
+//     const mobileElement = await findByText('Phone: 1234567899');
+//     expect(mobileElement).toBeTruthy();
+//   });
+// });
